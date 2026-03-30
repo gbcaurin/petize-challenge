4ae15b716ec55c58267e1ab9c703e69aafcf2e9c
Gabriel Caurin <gabrielcaurin@gmail.com>
2026-03-30 17:30:33 -0300

    feat: add infinite scroll repos on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { user } from "../services/github.js";
+import { user, repos } from "../services/github.js";
 import { userSchema } from "../schemas/user.js";
+import { repoSchema } from "../schemas/repository.js";
+import { z } from "zod";
 
 function Profile() {
   const navigate = useNavigate();
   const { username } = useParams();
   const [userData, setUserData] = useState(null);
+  const [reposData, setReposData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [moreRepos, setMoreRepos] = useState(true);
+  const [sort, setSort] = useState("created");
+  const obsRef = useRef();
 
   useEffect(() => {
     async function fetchUserData() {
@@ -26,6 +33,37 @@ function Profile() {
     fetchUserData();
   }, [username]);
 
+  useEffect(() => {
+    async function fetchReposData() {
+      try {
+        const reposData = await repos(username, page, sort);
+        const validate = z.array(repoSchema).safeParse(reposData);
+        if (!validate.success) {
+          console.error("Invalid repos data:", validate.error);
+          return;
+        }
+        setReposData((prev) => [...prev, ...validate.data]);
+        if (validate.data.length < 10) {
+          setMoreRepos(false);
+        }
+      } catch (error) {
+        console.error("Error fetching repos data:", error);
+      }
+    }
+
+    fetchReposData();
+  }, [username, page, sort]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && moreRepos) {
+        setPage((prev) => prev + 1);
+      }
+    });
+    observer.observe(obsRef.current);
+    return () => observer.disconnect();
+  }, [moreRepos]);
+
   return (
     <div className="profile">
       <h1>Profile</h1>
@@ -36,6 +74,21 @@ function Profile() {
           <p>Bio: {userData.bio}</p>
         </div>
       )}
+
+      <h2>Repos</h2>
+      {reposData.map((repo) => (
+        <div key={repo.id}>
+          <p>Name: {repo.name}</p>
+          <p>Description: {repo.description}</p>
+          <p>Stars: {repo.stargazers_count}</p>
+          <p>Forks: {repo.forks_count}</p>
+          <p>Language: {repo.language}</p>
+          <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+            View on GitHub
+          </a>
+        </div>
+      ))}
+      <div ref={obsRef} />
       <button onClick={() => navigate(-1)}>Voltar</button>
     </div>
   );
